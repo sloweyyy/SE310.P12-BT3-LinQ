5f0f8f24b9348cc3653c81be0ecab6a03296b6b5
sloweyyy <truonglevinhphuc2006@gmail.com>
2024-10-03 22:45:51 +0700

    Update LINQ_to_Object.sql

--- LINQ_to_Object.sql
+++ LINQ_to_Object.sql
@@ -1,4 +1,19 @@
-﻿CREATE TABLE Product
+﻿USE [SE310.P12-LinQ]
+
+select
+    'data source=' + @@servername +
+    ';initial catalog=' + db_name() +
+    case type_desc
+        when 'WINDOWS_LOGIN'
+            then ';trusted_connection=true'
+        else
+            ';user id=' + suser_name() + ';password=<<YourPassword>>'
+        end
+        as ConnectionString
+from sys.server_principals
+where name = suser_name()
+
+CREATE TABLE Product
 (
     ProductID INT PRIMARY KEY,
     ProductName NVARCHAR(50),
